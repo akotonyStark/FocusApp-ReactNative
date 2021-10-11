537c0a0f44dc8b79c35b8b7c213f31f0fd5ca945
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-10-11 09:58:59 +0000

    components added

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
+import Dummy from './components/Dummy'
 
 export default function App() {
+  const [isLoggedIn, setisLoggedIn] = useState(false)
   return (
     <View style={styles.container}>
       <StatusBar style='auto' />
-      <Button
-        title='Click to login'
-        style={{ backgroundColor: 'red' }}
-      ></Button>
+      {isLoggedIn ? (
+        <Dummy />
+      ) : (
+        <Button
+          title='Click to login'
+          style={{ backgroundColor: 'red' }}
+          onPress={() => {
+            setisLoggedIn(true)
+          }}
+        ></Button>
+      )}
       <Text>Welcome to Focus App</Text>
-      <View></View>
     </View>
   )
 }
---
+++ components/Dummy.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+
+function Dummy() {
+  const [count, setCount] = useState(0)
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 50 }}>{count}</Text>
+
+      <Button
+        title='Increment Counter'
+        style={{ color: 'red' }}
+        onPress={() => {
+          setCount(count + 1)
+          console.log(count)
+        }}
+      ></Button>
+      <Text>This is our dummy page</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
+
+export default Dummy
