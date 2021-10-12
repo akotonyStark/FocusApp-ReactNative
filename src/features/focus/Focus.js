7d31e82b93c5f86674c32b7541b53f4e635ad735
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-10-12 15:12:55 +0000

    timer

--- src/features/focus/Focus.js
+++ src/features/focus/Focus.js
@@ -3,15 +3,23 @@ import { Text, View, StyleSheet, Image } from 'react-native'
 import { TextInput } from 'react-native-paper'
 import { RoundedButton } from '../../components/RoundedButton'
 
-export default function Focus() {
+export default function Focus({ addSubject }) {
+  const [tempItem, setTempItem] = React.useState(null)
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>What would you like to focus on?</Text>
         <View style={styles.inputContainer}>
-          <TextInput style={{ flex: 1, marginRight: 10 }} />
+          <TextInput
+            style={{ flex: 1, marginRight: 10 }}
+            onSubmitEditing={({ nativeEvent }) => {
+              setTempItem(nativeEvent.text)
+            }}
+          />
           <RoundedButton
-            onPress={() => alert('hello')}
+            onPress={() => {
+              addSubject(tempItem)
+            }}
             size={50}
             title='+'
           ></RoundedButton>
