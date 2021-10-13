c07fb20be1a7e0cd53ab5b938bb6f8eed21e339d
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-10-13 15:31:54 +0000

    Update Timer.js

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,51 +1,68 @@
-import React from 'react'
-import { View, StyleSheet, Text, Button } from 'react-native'
-import { colors } from '../utils/colors'
-import { fontSizes } from '../utils/sizes'
-import Countdown from '../components/Countdown'
-import { RoundedButton } from '../components/RoundedButton'
+import React from 'react';
+import { View, StyleSheet, Text, Button } from 'react-native';
+import {ProgressBar} from 'react-native-paper'
+import { colors } from '../utils/colors';
+import { fontSizes } from '../utils/sizes';
+import Countdown from '../components/Countdown';
+import { RoundedButton } from '../components/RoundedButton';
+import {Timing} from './timing'
 
 export const Timer = ({ focusSubject }) => {
-  const [isStarted, setIsStarted] = React.useState(false)
+  const [isStarted, setIsStarted] = React.useState(false);
+  const [progress, setProgress] = React.useState(1)
+  const [minutes, setMinutes] = React.useState(0.1)
 
+  const onProgress = (progress) => {
+    setProgress(progress)
+  }
+
+ const changeTime = (min) => {
+   setMinutes(min)
+ }
   React.useEffect(() => {
-    console.log(isStarted)
+    //console.log(isStarted)
     return () => {
       //cleanup
-    }
-  }, [isStarted])
+    };
+  }, [isStarted]);
 
   return (
     <View style={styles.container}>
-      <Countdown isStarted={isStarted} />
-      <View>
-        {' '}
+      <Countdown isStarted={isStarted} onProgress = {onProgress}/>
+      <View>      
         <Text style={styles.title}>Focusing on: </Text>
         <Text style={styles.task}> {focusSubject} </Text>
+        <ProgressBar progress = {progress} color = "#5E84E2" style={{height: 20, marginTop: 20}} />
+      </View>
+      <View>
+          <Timing onChangeTime = {changeTime}/>
+      </View>
+      <View>
         <View style={styles.actions}>
           {isStarted ? (
             <RoundedButton
-              title='Pause'
+              title="Pause"
               onPress={() => {
-                setIsStarted(false)
+                setIsStarted(false);
               }}
             />
           ) : (
             <RoundedButton
-              title='Play'
+              title="Play"
               onPress={() => {
-                setIsStarted(true)
+                setIsStarted(true);
               }}
             />
           )}
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
+    marginTop:30,
     flex: 1,
   },
   title: {
@@ -62,4 +79,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 20,
   },
-})
+});
