6553ff25148381be609682b32c8d2c9237021019
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-10-14 16:24:22 +0000

    Update Timer.js

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,41 +1,54 @@
 import React from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
-import {ProgressBar} from 'react-native-paper'
+import { ProgressBar } from 'react-native-paper';
 import { colors } from '../utils/colors';
 import { fontSizes } from '../utils/sizes';
 import Countdown from '../components/Countdown';
 import { RoundedButton } from '../components/RoundedButton';
-import {Timing} from './timing'
+import { Timing } from './timing';
+import {useKeepAwake} from 'expo-keep-awake';
 
 export const Timer = ({ focusSubject }) => {
+  useKeepAwake();
+  
   const [isStarted, setIsStarted] = React.useState(false);
-  const [progress, setProgress] = React.useState(1)
-  const [minutes, setMinutes] = React.useState(0.1)
+  const [progress, setProgress] = React.useState(1);
+  const [minutes, setMinutes] = React.useState(1);
 
   const onProgress = (progress) => {
-    setProgress(progress)
-  }
+    setProgress(progress);
+  };
 
- const changeTime = (min) => {
-   setMinutes(min)
- }
+  const changeTime = (min) => {
+    setMinutes(min);
+    setProgress(1);
+    setIsStarted(false);
+  };
   React.useEffect(() => {
-    //console.log(isStarted)
+    console.log(minutes, isStarted);
     return () => {
       //cleanup
     };
-  }, [isStarted]);
+  }, [minutes, isStarted]);
 
   return (
     <View style={styles.container}>
-      <Countdown isStarted={isStarted} onProgress = {onProgress}/>
-      <View>      
+      <Countdown
+        isStarted={isStarted}
+        onProgress={onProgress}
+        minutes={minutes}
+      />
+      <View>
         <Text style={styles.title}>Focusing on: </Text>
         <Text style={styles.task}> {focusSubject} </Text>
-        <ProgressBar progress = {progress} color = "#5E84E2" style={{height: 20, marginTop: 20}} />
+        <ProgressBar
+          progress={progress}
+          color="#5E84E2"
+          style={{ height: 20, marginTop: 20 }}
+        />
       </View>
       <View>
-          <Timing onChangeTime = {changeTime}/>
+        <Timing onChangeTime={changeTime} />
       </View>
       <View>
         <View style={styles.actions}>
@@ -62,7 +75,7 @@ export const Timer = ({ focusSubject }) => {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:30,
+    marginTop: 30,
     flex: 1,
   },
   title: {
